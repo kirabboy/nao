a619df3c61776cc1a40c07530ee88cd59aad6774
kirabboy <kirabboytt@gmail.com>
2021-12-22 17:07:18 +0700

    fp

--- resources/views/public/layout/header.blade.php
+++ resources/views/public/layout/header.blade.php
@@ -7,11 +7,10 @@
     <title>Trang chủ</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('/css/header.css')}}">
+    <link rel="stylesheet" href="{{ asset('/css/header.css') }}">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
-    integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-
+        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="url-home" content="{{ URL::to('/') }}" />
 
@@ -39,15 +38,16 @@
                     </li>
                 </ul>
             </div>
-          </nav>
-          <div class="search-top">
+        </nav>
+        <div class="search-top">
             <div class="input-group mb-3 search-input">
                 <div class="input-group-prepend">
-                  <span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i>
+                    <span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i>
 
-                  </span>
+                    </span>
                 </div>
-                <input type="text" class="form-control" placeholder="Tìm kiếm..." aria-label="Username" aria-describedby="basic-addon1">
-              </div>
-          </div>
+                <input type="text" class="form-control" placeholder="Tìm kiếm..." aria-label="Username"
+                    aria-describedby="basic-addon1">
+            </div>
+        </div>
     </header>
