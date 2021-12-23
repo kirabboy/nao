192fbd0661aaa2b66c894f7801bb8ae725e0b45f
trantruong321 <tranvantruongtvtpro123@gmail.com>
2021-12-23 15:10:50 +0700

    upload layout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('public.index');
 });
+Route::get('chuyen-khoan', function () {
+    return view('public.payment');
+});
+Route::get('dang-nhap', function () {
+    return view('public.user.login');
+});
+Route::get('dang-nhap-otp', function () {
+    return view('public.user.login-otp');
+});
+Route::get('dang-ky', function () {
+    return view('public.user.register');
+});
+Route::get('otp', function () {
+    return view('public.user.otp');
+});
+Route::get('quen-mat-khau', function () {
+    return view('public.user.forget-password');
+});
+Route::get('dang-ky-thanh-cong', function () {
+    return view('public.user.register-success');
+});
