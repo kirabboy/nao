fc1c60f8f2aa6dcb972a459bad1ded38b0bbff67
nguyenphanquocthinh <44148967+nguyenphanquocthinh@users.noreply.github.com>
2021-12-24 15:49:56 +0700

    16:00

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Admin\LoginController;
+use App\Http\Controllers\HomeController; 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('admin/login', [LoginController::class, 'index']);
+Route::prefix('profile')->group(function () {
+    Route::get('/', [HomeController::class, 'index']);
+    Route::get('/info', [HomeController::class, 'infoDetail']);
+    Route::get('/resetPassword', [HomeController::class, 'resetPassword']);
+    Route::get('/thanhtoan', [HomeController::class, 'thanhtoan']);
+
+    Route::get('/doinhom', [HomeController::class, 'doinhom']);
+    Route::get('/chitietthanhvien', [HomeController::class, 'chitietthanhvien']);
+    
+    Route::get('/chiphi', [HomeController::class, 'chiphi']);
+    Route::get('/diemNAOnhanhtach', [HomeController::class, 'diemNAOnhanhtach']);
+    Route::get('/tongNAOtrongthang', [HomeController::class, 'tongNAOtrongthang']);
+    Route::get('/hoahong', [HomeController::class, 'hoahong']);
+    Route::get('/hoahongbanle', [HomeController::class, 'hoahongbanle']);
+    Route::get('/hoahongnhom', [HomeController::class, 'hoahongnhom']);
+
 });
