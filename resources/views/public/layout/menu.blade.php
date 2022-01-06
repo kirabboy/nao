9118094a3369be53c64b73d1f04c36350f38731c
unknown <vuquocminh99@gmail.com>
2022-01-06 09:58:22 +0700

    gan link menu

--- resources/views/public/layout/menu.blade.php
+++ resources/views/public/layout/menu.blade.php
@@ -1,14 +1,14 @@
 <div class="pt-86"></div>
 <div class="menu-fix d-flex justify-content-around">
-    <a class="d-flex justify-content-center align-items-center flex-column item">
+    <a class="d-flex justify-content-center align-items-center flex-column item" href="{{url('/')}}">
         <i class="fa fa-home" aria-hidden="true"></i>
         <span>Trang chủ</span>
     </a>
-    <a class="d-flex justify-content-center align-items-center flex-column item">
+    <a class="d-flex justify-content-center align-items-center flex-column item" href="{{route('product_daily')}}">
     <i class="fa fa-shopping-cart" aria-hidden="true"></i>
         <span>Sản phẩm</span>
     </a>
-    <a class="d-flex justify-content-center align-items-center flex-column item">
+    <a class="d-flex justify-content-center align-items-center flex-column item" href="{{url('/don-hang')}}">
         <i class="fa fa-cube" aria-hidden="true"></i>
         <span>Đơn hàng</span>
     </a>
